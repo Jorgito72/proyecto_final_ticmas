d9111ba0a7471cc02265843500dcd501c23315e8
Jorge Pablo <pablojorgel01@gmail.com>
2023-01-20 23:44:55 -0300

    se agregaron las funcionalidades con js

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,1 +1,49 @@
-v
+
+// funcion para mostrar articulo de experiencia
+document.getElementById("experiencia").addEventListener("click", function(){
+    document.getElementById('art_experiencia').style.display="block";
+    document.getElementById('art_skills').style.display="none";
+    document.getElementById('art_creaciones').style.display="none";
+    document.getElementById('art_contact').style.display="none";
+    document.getElementById('contenedor_central').style.display="none";
+    });
+
+// funcion para mostrar articulo de skills
+
+ document.getElementById("skills").addEventListener("click", function(){
+        document.getElementById('art_skills').style.display="block";
+        document.getElementById('art_experiencia').style.display="none"
+        document.getElementById('art_contact').style.display="none";
+        document.getElementById('art_creaciones').style.display="none";
+        document.getElementById('contenedor_central').style.display="none";
+        });
+
+ // funcion para mostrar articulo de creaciones
+
+ document.getElementById("creaciones").addEventListener("click", function(){
+    document.getElementById('art_creaciones').style.display="block";
+    document.getElementById('art_experiencia').style.display="none"
+    document.getElementById('art_contact').style.display="none";
+    document.getElementById('art_skills').style.display="none";
+    document.getElementById('contenedor_central').style.display="none";
+    });
+
+// funcion para mostrar articulo de contacto
+
+document.getElementById("contact").addEventListener("click", function(){
+    document.getElementById('art_contact').style.display="block";
+    document.getElementById('art_experiencia').style.display="none"
+    document.getElementById('art_creaciones').style.display="none";
+    document.getElementById('art_skills').style.display="none";
+    document.getElementById('contenedor_central').style.display="none";
+    });    
+
+ // funcion para mostrar articulo de contacto
+
+document.getElementById("header").addEventListener("click", function(){
+    document.getElementById('art_contact').style.display="none";
+    document.getElementById('art_experiencia').style.display="none"
+    document.getElementById('art_creaciones').style.display="none";
+    document.getElementById('art_skills').style.display="none";
+    document.getElementById('contenedor_central').style.display="block";
+    });       
